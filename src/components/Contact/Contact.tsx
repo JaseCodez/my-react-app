c8052cca9d46d1f6db19775ac0431abba58ed193
phanduc <jase.phan@mail.utoronto.ca>
2025-05-27 19:39:28 -0400

    Add text to show contact name

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,6 +1,7 @@
 import './Contact.css'
 import contact from '../../images/contact'
 import { IconType } from 'react-icons';
+import { useState } from 'react'; 
 
 interface Props {
     name: string, 
@@ -17,10 +18,25 @@ function Contact() {
         <div className='contact'>
             <div className='link-container'>
                 {contact.map((contact: Props)=>{
+                    const [ishover, setishover] = useState(false)
+
+                    function MouseOver() {
+                        setishover(true);
+                    }
+                    
+                    function MouseOut() {
+                        setishover(false); 
+                    }
                     return <div className='contact-row'>
-                        <a className='contact-link' href={contact.link} target='_blank'>
-                            <contact.Icon className='icon' size={45} />
-                        </a>
+
+                        <div className='hover-contact' onMouseOver={MouseOver} onMouseOut={MouseOut}>
+                            <a className='contact-link' href={contact.link} target='_blank'>
+                                <contact.Icon className='icon' size={45} />
+                            </a> { ishover && (
+                                <div className='contact-content'>{contact.content}</div>
+                            )}
+
+                        </div>
                     </div>
                 })}     
 
